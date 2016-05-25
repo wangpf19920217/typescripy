17b7a06f63a75719e5dbd602881f0d3be13f51f1
Wpf <851118434@qq.com>
2016-05-25 11:17:49 +0800

    关联git

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 // let isDone: boolean = false;
-// 元组的定义方式
-let x: [string, number];
-x = ['字符串',10];
-console.log(x);
+// // 元组的定义方式
+// let x: [string, number];
+// x = ['字符串',10];
+// console.log(x);
+//关联git
